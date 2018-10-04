fedc403b86d83d1a55d281d1686400c7c6437548
Hung <bapdola304@gmail.com>
2018-10-04 10:05:43 +0700

    save code

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -4,6 +4,9 @@ module.exports.addCart = function(req, res, next) {
 var idproduct = req.params.idproduct;
 var sessionId = req.cookies.sessionId;
 
+  
+
+
 if (!sessionId) {
     res.redirect('/products');
     return;
@@ -21,29 +24,4 @@ db.get('session')
 
 //res.locals.carts = carts;
 res.redirect('/products');
-}
-module.exports.carts = function(req, res, next) {
-
-var idproduct = req.params.idproduct;
-var sessionId = req.cookies.sessionId;
-var countCart = db.get('session').find({
-    id: sessionId
-}).get('cart').value();
-
-var a = [];
-for (var key in countCart) {
-    a.push(countCart[key]);
-}
-
-
-var carts = a.reduce(function(a, b) {
-    return a + b;
-});
-
-
-
-
-res.locals.carts = carts;
-next();
-
 }
--- middlewares/cart.middleware.js
+++ middlewares/cart.middleware.js
@@ -1,23 +1,36 @@
 var db = require('../db.js');
+var product = require('../models/product.model');
+module.exports.carts =  async function(req, res, next) {
+var idproduct = req.params.idproduct;
 
-module.exports.carts = function(req, res, next) {
 
-// vidproduct = req.params.idproduct;
+
 var sessionId = req.cookies.sessionId;
 var countCart = db.get('session').find({
     id: sessionId
 }).get('cart').value();
 if (!countCart) {
     res.locals.carts = 0;
+    res.locals.infocarts = 0;
+    console.log(res.locals.infocarts);
     next();
     return;
 }
 var a = [];
-
+var b = [];
 for (var key in countCart) {
     a.push(countCart[key]);
+     b.push(key);
 }
 
+if(b !== null){
+    var d = [];
+	for(var i=0; i< b.length ;i++){
+	d.push(await product.find({_id : b[i]}));
+	d.push({"soluong" : a[i]});
+	res.locals.infocarts = d;
+	}
+}
 
 var carts = a.reduce(function(a, b) {
     return a + b;
@@ -25,8 +38,9 @@ var carts = a.reduce(function(a, b) {
 
 
 
-
+res.locals.soluong = a;
 res.locals.carts = carts;
+
 next();
 
 }
